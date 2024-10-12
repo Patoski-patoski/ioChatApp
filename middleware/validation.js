28bfdafc81c5578ae95c372d6d3622e195ff8f4b
Okafor Patrick C <patrickpeters911@gmail.com>
2024-10-12 21:44:40 +0100

    Fix error in middleware/validation.js

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,23 +5,28 @@ export function validateSignupInput(req, res, next) {
     const { username, sex, password, confirm_password } = req.body;
 
     if (!username || !sex || !password) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Missing required fields' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Missing required fields' });
     }
 
     if (typeof username !== 'string' || username.length < 3) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Username must be a string with at least 3 characters' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Username must be a string with at least 3 characters' });
     }
 
     if (typeof password !== 'string' || password.length < 6) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Password must be a string with at least 6 characters' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Password must be a string with at least 6 characters' });
     }
 
     if (confirm_password !== password) {
-        return res.status(HTTP_STATUS.CONFLICT).json({ error: "Password does not match" });
+        return res.status(HTTP_STATUS.CONFLICT).json(
+            { error: "Password does not match" });
     }
 
     if (sex !== 'male' && sex !== 'female' && sex !== 'other') {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Sex must be either "male", "female", or "other"' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Sex must be either "male", "female", or "other"' });
     }
     next();
 }
@@ -29,15 +34,18 @@ export function validateLoginInput(req, res, next) {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Missing required fields' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Missing required fields' });
     }
 
     if (typeof username !== 'string' || username.length < 3) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Username must be a string with at least 3 characters' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Username must be a string with at least 3 characters' });
     }
 
     if (typeof password !== 'string' || password.length < 6) {
-        return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Password must be a string with at least 6 characters' });
+        return res.status(HTTP_STATUS.BAD_REQUEST).json(
+            { error: 'Password must be a string with at least 6 characters' });
     }
     next();
 }
